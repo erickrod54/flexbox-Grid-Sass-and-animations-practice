22e600d2c8a40c9fa15f9149a74d3bd58f12dfcb
Erick Rodriguez <erickrod54@gmail.com>
2022-05-05 22:22:35 -0400

    Flexbox-grid-sass-and-animation app version 3 - flexbox.styled.components - Features: Implementing justify-content and flex together, Implementing inline-flex and creating a class for .ul-flex the second children

--- src/setup-app/flexbox-app/flexbox.styled.components.js
+++ src/setup-app/flexbox-app/flexbox.styled.components.js
@@ -1,21 +1,50 @@
 import styled from 'styled-components'
 
-/**Flexbox-grid-sass-and-animations app version 2 -
+/**Flexbox-grid-sass-and-animations app version 3 -
  * flexbox.styled.components - Features:
  * 
- *      -->Building 'Wrapper' Style
- *        Compoment.
+ *      -->Implementing 'justify-content' and 'flex' 
+ *         together.
  * 
- * Note:This component will be use to wrap complex-title
+ *      -->Implementing 'inline-flex' and creating a
+ *         class for '.ul-inline-flex' the second 
+ *         children.
+ * 
+ * Note: display 'flex' is different to 'inline-block' 
+ * because 'flex' give me more control over the items (as
+ * is the case of applying 'justify-content' thanks to
+ * flex i can control the 'children' with this prop)
+ * 
+ * The difference of the 'inline-flex' and 'display-flex'
+ * is that 'inline-flex', diplay flex is for 'block level'
+ * containers
+ * 
+ * 'justify-content' and 'inline-flex' are properties
+ * that are activated when display 'flex' is set, 
+ * otherwise using 'inline-block' these last don't have
+ * effect 
  */
+
 export const FlexBoxWrapper = styled.div`
+    /**Flex container => parent */
     ul {
         background-color: lightslategray;
         padding: 20px;
+        /**Flex container properties */
         display: ${({ toggleFlex }) => 
         toggleFlex || 'flex' };
+        /**this props set everything to the center */
+        justify-content: center;
     }
-
+    /**Here i implement the 'inline-flex' */
+    .ul-inline-flex {
+        background-color: lightslategray;
+        padding: 20px;
+        /**Flex container properties */
+        display: inline-flex;
+        justify-content: center;
+    }
+    /**Flex Items => children */
     li{
         list-style: none;
         color: white;
@@ -32,9 +61,10 @@ export const FlexBoxWrapper = styled.div`
 `
 
 export const Wrapper = styled.div`
-    h1{
+    h2{
         text-transform: capitalize;
         text-align: start;
+        font-size: 1.75rem;
     }
     .flexbox-span{
         color: blueviolet;
