54a01ed1e2759647dd6e672af99d4fd82cd2a1f5
Erick Rodriguez <erickrod54@gmail.com>
2022-05-09 21:22:58 -0400

    Flexbox-grid-sass-and-animations app version 5 - data js - Features: Building data array to serve to the Section Component

---
+++ src/data.js
@@ -0,0 +1,27 @@
+/**Flexbox-grid-sass-and-animations app version 5 -
+ * data js - Features:
+ * 
+ *      -->Building data array to serve to the 'Section' 
+ *        Component
+ * 
+ * Note: In this version i'll start to do flex manipulation
+ * of children elements, this case is made base on a section
+ * list of elements.
+ */
+export const sectiondata = [
+    {
+       id: 'child-1',
+       title:'Section 1',
+       text: 'Lorem ipsum dolor sit  amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'     
+    },
+    {
+       id: 'child-2',
+       title:'Section 2',
+       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' 
+    },
+    {
+        id: 'child-3',
+        title:'Section 3',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+]
