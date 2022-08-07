b02fe10fde19281c2df5c4893f628ea9adaa0b44
Erick Rodriguez <erickrod54@gmail.com>
2022-08-07 17:59:00 -0400

    Flexbox-grid-sass-and-animations app version 8 - index js file - Features: Importing and Wrapping the whole app using AppProvider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AppProvider } from './context';
+
+/**Flexbox-grid-sass-and-animations app version 8 -
+ * index js file - Features:
+ * 
+ *      -->Importing and Wrapping the whole app
+ *         using 'AppProvider'        
+ * 
+ * Note: This way the 'AppProvider' will provide
+ * data and features to the app.
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
 
