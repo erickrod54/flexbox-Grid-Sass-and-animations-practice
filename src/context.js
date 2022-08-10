a41ed4aa7aca17b22350686ee98a46cc61ae42ee
Erick Rodriguez <erickrod54@gmail.com>
2022-08-10 02:54:24 -0400

    Flexbox-grid-sass-and-animations app version 9 - context js file - Features: Building props in the initialState for flexbox-app and flexbox manipulation, Dispathcing TOGGLE_FLEX action, Dispatching TOGGLE_FLEX_MANIPULATION_APP action, Providing toggleFlex nad toggleFlexManApp throught the provider

--- src/context.js
+++ src/context.js
@@ -1,20 +1,21 @@
 import React, { useContext, useReducer } from "react";
+import { TOGGLE_FLEX, TOGGLE_FLEX_MANIPULATION_APP } from './actions'
 import reducer from "./reducer";
 
-/**Flexbox-grid-sass-and-animations app version 8 -
+/**Flexbox-grid-sass-and-animations app version 9 -
  * context js file - Features:
  * 
- *      -->Building 'AppContext' for the whole app.
+ *      --> Building props in the initialState for 
+ *         'flexbox-app' and 'flex-box manipulation'.
  * 
- *      -->Building 'AppProvider' for the whole app.
+ *      --> Dispatching 'TOGGLE_FLEX' action.
  * 
- *      -->Building 'useAppContext' hook 
- * 
- *      -->Building 'initialState' to provide a test
- *         message
- * 
- *      -->Building state to use the reducer and start
- *         to build the actions.       
+ *      --> Dispatching 'TOGGLE_FLEX_MANIPULATION_APP'
+ *          action.
+ *  
+ *      --> Providing 'toggleFlex' and 
+ *         'toggleFlexManApp' througt the 
+ *          provider.       
  * 
  * Note: now is pending to migrate all the data and
  * features and provide it througt the provider.
@@ -22,7 +23,9 @@ import reducer from "./reducer";
 
 /**here i build the initialState */
 const initialState = {
-    testMsg: 'success test context for flexbox-app',
+    /**Props for flexbox-app and flex-box manipulation */
+    flex: true,
+    flexmanapp:true
 }
 
 /**here i build the AppContext */
@@ -34,10 +37,22 @@ const AppProvider = ({ children }) => {
     /**here i build the state to dispatch the actions */
     const [ state, dispatch ] = useReducer( reducer, initialState)
 
+   const toggleFlex = () => {
+    dispatch({ type: TOGGLE_FLEX, payload: state.flex})
+   }
+   
+   const toggleFlexManApp = () => {
+    dispatch({ type: TOGGLE_FLEX_MANIPULATION_APP, payload: state.flexmanapp})
+   }
+
     /**i spread/copy the 'state' to test the context */
     return(
         <AppContext.Provider 
-            value={{...state}}
+            value={{
+                ...state,
+                toggleFlex,
+                toggleFlexManApp
+            }}
             >{children}
         </AppContext.Provider>
     )
