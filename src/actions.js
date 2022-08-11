6dbe1100b1967c5579ebb428590e8f2d942f4e2a
Erick Rodriguez <erickrod54@gmail.com>
2022-08-11 12:17:41 -0400

    Flexbox-grid-sass-and-animations app version 9 - actions js file - Features: Building variable for TOGGLE_FLEX action, Building variable for TOGGLE_FLEX_MANIPULATION_APP action

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+/**Flexbox-grid-sass-and-animations app version 9 -
+ * actions js file - Features:
+ * 
+ *      --> Building variable for 
+ *         'TOGGLE_FLEX' action
+ * 
+ *      --> Building variable for 
+ *         'TOGGLE_FLEX_MANIPULATION_APP' 
+ *          action            
+ * 
+ * Note: By this version and on will be build
+ * in this file the variables with the actions
+ * names for this application
+ * 
+ */
+
+export const TOGGLE_FLEX = 'TOGGLE_FLEX';
+export const TOGGLE_FLEX_MANIPULATION_APP = 'TOGGLE_FLEX_MANIPULATION_APP';
+ 
