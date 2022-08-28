8affe0e74c74b393037e4abdde59d7d4d2bd95b6
Erick Rodriguez <erickrod54@gmail.com>
2022-08-28 04:34:05 -0400

    Flexbox-grid-sass-and-animations app version 10 - App js file - Features: Destructuring handleChange, direction, handleWrap, and wrap from useAppContext()

--- src/App.js
+++ src/App.js
@@ -2,62 +2,23 @@ import React from 'react'
 import FlexBoxApp from './setup-app/flexbox-app/flexbox-app';
 import FlexBoxManipulationApp from './setup-app/flexbox-app/flexbox.manipulation-app';
 import FlexPropApp from './setup-app/flexbox-app/flex.direction.property-app';
-import { useState } from 'react';
 import { useAppContext } from './context'
 
-/**Flexbox-grid-sass-and-animations app version 8 -
+/**Flexbox-grid-sass-and-animations app version 10 -
  * App js file - Features:
  * 
- *      --> Destructuring 'data' from 
- *         'useAppContext()' to test 
- *          the context. 
+ *      --> Destructuring 'handleChange', 'direction', 
+ *          'handleWrap' and 'wrap' from 
+ *           'useAppContext()'   
  * 
- * Note: by this version is starting the
- * context arquitecture implementation.
+ * Note: Migrating handleChange, handleWrap and their
+ * states to the context  
+ * 
+ * pending to build actions for them.
  */
 function App() {
 
-  const data = useAppContext()
-
-  console.log('provided from AppProvider ==> ', data)
-  /**here i build the wrap state */
-  const [ wrap, setWrap ] = useState({
-    nowrap: 'nowrap',
-    wrap:'wrap',
-    wrapreverse:'wrap-reverse'
-  })
-
-  /**here i build the direction state */
-  const [ direction, setDirection ] = useState({
-    row: 'row',
-    column:'column',
-    rowreverse: 'row-reverse',
-    columnreverse:'column-reverse'
-})
-
-/**here i build the 'handleWrap' feature*/
-const handleWrap = (e) => {
-  const name = e.target.name;
-  const value = e.target.value;
-
-  /**here i test the selection using the event object*/
-  console.log('wrap => name selected ==>', name, ', value in it ==>', value)
-
-  setWrap({...wrap, [name]:value })
-}
-
-/**here i build the 'handleChange' feature
- * should be 'handleDirection'
-*/
-const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-
-    /**here i test the selection using the event object*/
-    console.log('direction => name selected ==>', name, ', value in it ==>', value)
-
-    setDirection({...direction, [name]:value})
-}
+  const { handleChange, direction, handleWrap, wrap } = useAppContext()
 
   return (
     <>
