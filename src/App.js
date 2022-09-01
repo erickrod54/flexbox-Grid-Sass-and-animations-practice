c430e8b151330248c5bea7732092a144f9c47f89
Erick Rodriguez <erickrod54@gmail.com>
2022-09-01 01:00:02 -0400

    Flexbox-grid-sass-and-animations app version 12 - App js file - Features: Building routing to take for every app element, Importing and Placing AppPage as a shared layout for all the apps

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import React from 'react'
-import FlexBoxApp from './setup-app/flexbox-app/flexbox-app';
-import FlexBoxManipulationApp from './setup-app/flexbox-app/flexbox.manipulation-app';
-import FlexPropApp from './setup-app/flexbox-app/flex.direction.property-app';
-import FlexFlow from './setup-app/flexbox-app/flexbox.flow-app';
 
-/**Flexbox-grid-sass-and-animations app version 11 -
+import { 
+         HomePage,
+         FlexBoxApp,
+         FlexBoxManipulationApp,
+         FlexPropApp,
+         FlexFlow,
+         AppPage
+        } from './setup-app/flexbox-apps-list/index-apps.list'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+/**Flexbox-grid-sass-and-animations app version 12 -
  * App js file - Features:
  * 
- *      --> Taking off the prop drills at 
- *          'FlexPropApp'
+ *      --> Building routing to take for every
+ *          'AppElement' build
  * 
- * Note: With this change will be easier to implement
- * routing to seperate the apps into sections ( this
- * changes is going to be in next versions)
+ *      --> Importing and Placing 'AppPage' as
+ *          a shared layout for all the apps
+ * 
+ * Note: This routing will help to get into every
+ * app separately.
+ * 
+ * in order to set the shared layout i have to 
+ * change the previous routes from '/' to '/apps'
+ * so it won't create a conflict 
  */
 
 
 function App() {
 
   return (
-    <>
-      <h2>The Modern Flexbox, Grid, Styles and Animations</h2>
-      <div style={{ padding: '2rem'}}>
-        <FlexBoxApp />
-        <FlexBoxManipulationApp />
-        <FlexPropApp />
-        <FlexFlow />
-      </div>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        
+        <Route path='/apps' element={<AppPage />}>
+        <Route path='/apps/flexbox-app' element={<FlexBoxApp />}/>
+        <Route path='/apps/flexbox-manipulation-app' element={<FlexBoxManipulationApp />}/>
+        <Route path='/apps/flexbox-prop-app' element={<FlexPropApp />}/>
+        <Route path='/apps/flexbox-flow-app' element={<FlexFlow />}/> 
+
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
