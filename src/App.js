2c3eeaa6fb12bcf072c60b7855710f1fe750701a
Erick Rodriguez <erickrod54@gmail.com>
2022-09-11 00:14:39 -0400

    Flexbox-grid-sass-and-animations app version 13 - App js file - Features: Importing JustifyContentApp, AlignItemsApp and AlignContentApp Components and building the route for it.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import {
          FlexBoxManipulationApp,
          FlexPropApp,
          FlexFlow,
-         AppPage
+         AppPage,
+         JustifyContentApp,
+         FlexAlignItemsApp ,
+         FlexAlignContentApp
         } from './setup-app/flexbox-apps-list/index-apps.list'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-/**Flexbox-grid-sass-and-animations app version 12 -
+/**Flexbox-grid-sass-and-animations app version 13 -
  * App js file - Features:
+ *
+ *      --> Importing 'JustifyContentApp' Component 
+ *          and building the route for it.
  * 
- *      --> Building routing to take for every
- *          'AppElement' build
+ *      --> Importing 'AlignItemsApp' Component 
+ *          and building the route for it.
  * 
- *      --> Importing and Placing 'AppPage' as
- *          a shared layout for all the apps
+ *      --> Importing 'AlignContentApp' Component 
+ *          and building the route for it.
  * 
  * Note: This routing will help to get into every
  * app separately.
@@ -41,7 +47,9 @@ function App() {
         <Route path='/apps/flexbox-manipulation-app' element={<FlexBoxManipulationApp />}/>
         <Route path='/apps/flexbox-prop-app' element={<FlexPropApp />}/>
         <Route path='/apps/flexbox-flow-app' element={<FlexFlow />}/> 
-
+        <Route path='/apps/flex-justify-content-app' element={<JustifyContentApp />}/>
+        <Route path='/apps/flex-align-items-app' element={<FlexAlignItemsApp />}/>
+        <Route path='/apps/flex-align-content-app' element={<FlexAlignContentApp />}/>  
         </Route>
       </Routes>
     </Router>
