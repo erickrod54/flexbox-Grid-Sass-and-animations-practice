9961b25ef8324a08c7562ed0443f2825b661061e
Erick Rodriguez <erickrod54@gmail.com>
2022-10-07 01:59:59 -0400

    Flexbox-grid-sass-and-animations app version 13 - FlexFlowContnt Component - Features: Building JustifyContent app

---
+++ src/components/justifyflex-content.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { useAppContext } from "../context";
+
+/**Flexbox-grid-sass-and-animations app version 13 -
+ * 'FlexFlowContent' Component - Features:
+ * 
+ *      --> Building 'JustifyContent' app 
+ * 
+ * Note: By this version everything is done in
+ * this component.
+ */
+
+const JustifyContent = ({ justify }) => {
+
+    const { JustifyFlexData } = useAppContext()
+
+    return(
+        <section>
+            
+            {/**i have to 'filter' first to get the id that matches with
+             * the 'flow.selection' and then i map to render the resulting 
+             * array*/}
+
+            {JustifyFlexData
+            .filter((item) => item.id === justify.selection ).map((itemjustify) => {
+                const { id, text } = itemjustify
+
+                return(
+                    <article key={id}>
+                        <h4>You selected: '{id}'</h4>
+                        <p>{text}</p>
+                    </article>
+                ) 
+            })}
+          
+                
+        </section>
+    )
+}
+
+export default JustifyContent;
