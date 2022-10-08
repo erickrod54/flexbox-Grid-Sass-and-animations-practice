99b856b0b1c6e9fed96b6e2e3fddac1092f774ea
Erick Rodriguez <erickrod54@gmail.com>
2022-10-08 01:44:47 -0400

    Flexbox-grid-sass-and-animations app version 13 -FlexFlow app - Features: Moving flex.flow-app jsx to the new app list directory

---
+++ src/setup-app/flexbox-apps-list/flexbox.flow-app.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FlexboxFlowWrapper } from "../flexbox.styled.components";
+import ComplexTitle from "../../components/complex-title";
+import FlexBoxWrap from '../../components/flex-prop-wrap.component';
+import FlexFlowContent from "../../components/flexflow-content.component";
+import { useAppContext } from  '../../context';
+
+/**Flexbox-grid-sass-and-animations app version 13 -
+ * 'FlexFlow' app - Features:
+ * 
+ *     --> Moving 'flexbox.flow-app.jsx' to
+ *          a the new 'flexbox-apps-list directory' 
+ * 
+ * Note: the only prop that i drill is for the Style Component
+ * to give the value that needs to change dynamiclly and apply
+ * the 'flow' style by the selection
+ */
+
+const FlexFlow = () => {
+
+    /**here i destructure props */
+    const { flow, handleFlow } = useAppContext() 
+
+    return(
+        <>
+        {/**here i build the custom title */}
+        <ComplexTitle 
+            title={<p><span className="flexbox-properties">
+            flexbox-flow
+        </span> for containers:</p>}/>
+
+            <h2>flex flow properties</h2>
+            <p>flex flow is a mix flexwrap and flex prop fetaures</p>
+        
+        {/**here select to show the options to the user */}
+        <select
+            name='selection'
+            id='selection'
+            value={ flow }
+            onChange={handleFlow}>
+                <option value="">Select a direction</option>
+                <option value="row wrap">row wrap</option>
+                <option value="column wrap">column wrap</option>
+                <option value="row-reverse wrap">row-reverse wrap</option>
+                <option value="column-reverse wrap">column-reverse wrap</option>
+                <option value="row nowrap">row nowrap</option>
+                <option value="column nowrap">column nowrap</option>
+                <option value="row-reverse nowrap">row-reverse nowrap</option>
+                <option value="column-reverse nowrap">column-reverse nowrap</option>
+            </select>
+        
+        <FlexFlowContent />
+        
+        <FlexboxFlowWrapper flow={flow}>
+            <FlexBoxWrap />
+        </FlexboxFlowWrapper>
+        </>
+
+    )
+}
+
+export default FlexFlow;
