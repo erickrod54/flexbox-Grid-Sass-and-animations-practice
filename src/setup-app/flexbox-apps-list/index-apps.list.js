d2a4e9c8c0bbf9d641e86165331fb21a634dc84d
Erick Rodriguez <erickrod54@gmail.com>
2022-10-08 01:49:08 -0400

    Flexbox-grid-sass-and-animations app version 13 - index js file - Features: Adding JustifyContentApp, FlexAlignItemApp, and FlexAlignContentApp to the index

---
+++ src/setup-app/flexbox-apps-list/index-apps.list.js
@@ -0,0 +1,32 @@
+import HomePage from "../../pages/home-page";
+import AppPage from "../../pages/app-page";
+
+import FlexBoxApp from './flexbox-app';
+import FlexBoxManipulationApp from "./flexbox.manipulation-app";
+import FlexPropApp from './flex.direction.property-app';
+import FlexFlow from './flexbox.flow-app';
+import FlexAlignItemsApp from "./flex.align.items-app";
+import JustifyContentApp from "../flex-justify-content-app";
+import FlexAlignContentApp from './flex.align.content-app'
+
+/**Flexbox-grid-sass-and-animations app version 13 -
+ * index js file - Features:
+ * 
+ *      --> Adding 'JustifyContentApp', 'FlexAlignItemsApp',
+ *         ' FlexAlignContentApp'  
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+export { 
+         HomePage,
+         FlexBoxApp,
+         FlexBoxManipulationApp,
+         FlexPropApp,
+         FlexFlow,
+         AppPage,
+         JustifyContentApp,
+         FlexAlignItemsApp,
+         FlexAlignContentApp 
+        }
