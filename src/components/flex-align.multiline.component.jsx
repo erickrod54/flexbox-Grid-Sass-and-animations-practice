600938263fc50aa48b8a61fca7ea8e021e2c710b
Erick Rodriguez <erickrod54@gmail.com>
2022-10-09 00:43:21 -0400

    Flexbox-grid-sass-and-animations app version 7 - FlexAlignMultiline - Features: Building FlexAlignMultiline Component - this component is use to test align content props-

---
+++ src/components/flex-align.multiline.component.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+/**Flexbox-grid-sass-and-animations app version 7 -
+ * 'flex-prop-wrap.component' - Features:
+ * 
+ *      -->Building 'FlexAlignMultiline' Component .
+ * 
+ * Note: This is the longest version of FlexPropApp
+ * in order to demonstrate how 'wrap' and style multine
+ * items container.
+ */
+
+const FlexAlignMultiline = () => {
+
+    return(
+        <>
+            {/**This is the Parent element*/}
+            <ul>
+                {/**These are the children element*/}
+                <li>1st child</li>
+                <li>2nd child</li>
+                <li>3rd child</li>
+                <li>4th child</li>
+                <li>5th child</li>
+                <li>6st child</li>
+                <li>7nd child</li>
+                <li>8rd child</li>
+                <li>9th child</li>
+                <li>10th child</li>
+                <li>11th child</li>
+                <li>12th child</li>
+                <li>13th child</li>
+                <li>14th child</li>
+                <li>16th child</li>
+                <li>17th child</li>
+                <li>18th child</li>
+                <li>19th child</li>
+                <li>20th child</li>
+            </ul>
+        </>
+    )
+}
+
+export default FlexAlignMultiline;
