cbaa9f8e2e5bafb80d66b63b553ce4f45144eff7
Erick Rodriguez <erickrod54@gmail.com>
2022-10-10 02:42:43 -0400

    Flexbox-grid-sass-and-animations app version 15 - flex-prop-wrap.component - Features: Building Perfect center Component

---
+++ src/components/perfect.center.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+/**Flexbox-grid-sass-and-animations app version 15 -
+ * 'flex-prop-wrap.component' - Features:
+ * 
+ *      -->Building 'Perfect center' Component.
+ * 
+ * Note: The styles for this component are going to be set
+ * on 'AlignContentAppWrapper', the 'align-content' prop 
+ * make possible the perfect center.
+ */
+
+const PerfectCenter = () => {
+
+    return(
+        <>
+            {/**This is the Parent element*/}
+            <ul>
+                {/**These are the children element*/}
+                <li>1st child</li>
+            </ul>
+        </>
+    )
+}
+
+export default PerfectCenter;
