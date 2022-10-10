f832c60dd794ff5a12fe373f0bd2a58f4ee100cb
Erick Rodriguez <erickrod54@gmail.com>
2022-10-10 02:40:05 -0400

    Flexbox-grid-sass-and-animations app version 15 - flexbox.direction.property-app - Features: Destructuring props direction and handleChange from useAppContext() to set direction functionality, Building the return and drilling the props to control css and get state data props, Importing and placing PerfectCenter Component

--- src/setup-app/flexbox-apps-list/flex.align.content-app.jsx
+++ src/setup-app/flexbox-apps-list/flex.align.content-app.jsx
@@ -3,21 +3,24 @@ import ComplexTitle from "../../components/complex-title";
 import FlexAlignMultiline from "../../components/flex-align.multiline.component";
 
 import { useAppContext } from "../../context";
-import { AlignContentAppWrapper } from "../flexbox.styled.components";
+import { AlignContentAppWrapper, PerfectCenterWrapper } from "../flexbox.styled.components";
+
+import PerfectCenter from "../../components/perfect.center.component";
 
 
-/**Flexbox-grid-sass-and-animations app version 14 -
+/**Flexbox-grid-sass-and-animations app version 15 -
  * 'flexbox.direction.preperty-app' - Features:
  * 
- *      --> Destructuring props to build 
- *          'FlexAlignItemsApp' functionality.
- * 
- *      --> Importing and placing 'ComplexTitle'
- *          to set the main-title.
+ *      --> Destructuring props 'direction' and 
+ *          'handleChange' from 'useAppContext()'
+ *           to set direction functionality.
  * 
  *      --> Building the return and drilling props
  *          to control css and get state 'data' 
  *          props
+ * 
+ *      --> Importing and placing 'PerfectCenter' 
+ *          component.
  * 
  * Note: By this version is everything done for this 
  * component.
@@ -25,13 +28,14 @@ import { AlignContentAppWrapper } from "../flexbox.styled.components";
 
 const FlexAlignContentApp = () => {
 
-    const { handleAlignContent, aligncontent } = useAppContext()
+    const { handleAlignContent, aligncontent, direction, handleChange } = useAppContext()
+
     return(
         <>
         <ComplexTitle title={<h2><span className="flex-align-content-app">align content app: </span></h2>}/>
-        <h2>Is mix between 'justify-content' and 'align-items', is made for a 'multiline' flex-container
+        <h2>Is mix between 'justify-content' and 'align-content', is made for a 'multiline' flex-container
         ( a large collection of items inside of the container ), for all these props must be set the 'wrap' prop, to see how it 
-        take effect also needs a 'height' ( this this container has 600px )</h2>
+         take effect also needs a 'height' ( this this container has 600px )</h2>
 
         <select 
                 name="selection" 
@@ -41,7 +45,7 @@ const FlexAlignContentApp = () => {
                     {/**the first option with empty value is to 
                      * avoid 'row' as default option - is a select 
                      * tag behavior*/}
-                    <option value="">Select an align-items</option>1
+                    <option value="">Select an align-content</option>1
                     <option value='stretch'>stretch</option>
                     <option value='flex-start'>flex-start</option>
                     <option value='flex-end'>flex-end</option>
@@ -51,9 +55,47 @@ const FlexAlignContentApp = () => {
                     <option value='space-evenly'>space-evenly</option>
                 </select>
 
-        <AlignContentAppWrapper aligncontent={aligncontent}>
-           <FlexAlignMultiline />          
+             <h2>And if i change the direction, i change the 'cross' and 'main' axis orientation</h2>   
+                <select 
+                name="selection" 
+                id="selection" 
+                value={direction} 
+                onChange={handleChange}>
+                    {/**the first option with empty value is to 
+                     * avoid 'row' as default option - is a select 
+                     * tag behavior*/}
+                    <option value="">Select a direction</option>1
+                    <option value='row'>row</option>
+                    <option value='column'>column</option>
+                    <option value='row-reverse'>row-reverse</option>
+                    <option value='column-reverse'>column-reverse</option>
+            </select>
+            
+        <AlignContentAppWrapper aligncontent={aligncontent} direction={direction}>
+        <h4>You're combining : 
+        align-content:   
+         <span className="first-prop">
+            '{aligncontent.selection ? aligncontent.selection : 'select a prop'}'
+         </span>            
+             + 
+         direction:    
+         <span className="second-prop">
+             '{direction.selection ? direction.selection : 'select a prop'}'
+         </span>
+        </h4>               
+           <FlexAlignMultiline />  
+        
         </AlignContentAppWrapper>        
+
+         <h2>Also, for many coding interviews come up perfect center and is made by
+         'justify-content: center;' and 'align-items: center' in the container</h2>            
+
+         {/**look inside of this style component to see how
+          * 'perfect center' is made*/}            
+        <PerfectCenterWrapper>
+            <PerfectCenter />           
+        </PerfectCenterWrapper>
+        
         </>
     )
 }
