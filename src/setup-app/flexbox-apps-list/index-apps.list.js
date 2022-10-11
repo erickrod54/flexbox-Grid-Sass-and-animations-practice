76df9e2625008a0905f9960d35704d5afa6e959d
Erick Rodriguez <erickrod54@gmail.com>
2022-10-11 12:30:15 -0400

    Flexbox-grid-sass-and-animations app version 16 - index js file - Features: Adding OrderPropertyApp

--- src/setup-app/flexbox-apps-list/index-apps.list.js
+++ src/setup-app/flexbox-apps-list/index-apps.list.js
@@ -8,25 +8,27 @@ import FlexFlow from './flexbox.flow-app';
 import FlexAlignItemsApp from "./flex.align.items-app";
 import JustifyContentApp from "../flex-justify-content-app";
 import FlexAlignContentApp from './flex.align.content-app'
+import OrderPropertyApp from "./order.property-app";
 
-/**Flexbox-grid-sass-and-animations app version 13 -
+/**Flexbox-grid-sass-and-animations app version 16 -
  * index js file - Features:
  * 
- *      --> Adding 'JustifyContentApp', 'FlexAlignItemsApp',
- *         ' FlexAlignContentApp'  
+ *      --> Adding 'OrderPropertyApp'  
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
+
 export { 
-         HomePage,
-         FlexBoxApp,
-         FlexBoxManipulationApp,
-         FlexPropApp,
-         FlexFlow,
-         AppPage,
-         JustifyContentApp,
-         FlexAlignItemsApp,
-         FlexAlignContentApp 
+    HomePage,
+    FlexBoxApp,
+    FlexBoxManipulationApp,
+    FlexPropApp,
+    FlexFlow,
+    AppPage,
+    JustifyContentApp,
+    FlexAlignItemsApp,
+    FlexAlignContentApp,
+    OrderPropertyApp 
         }
