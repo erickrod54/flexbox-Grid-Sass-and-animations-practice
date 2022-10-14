00cc69cdc449ffb09bf05a5f248a8986c9774af6
Erick Rodriguez <erickrod54@gmail.com>
2022-10-14 04:10:14 -0400

    Flexbox-grid-sass-and-animations app version 20 - FlexShrinkPropertyApp - Features: Building FlexShrinkPropertyApp, Destructuring props from context, Testing shrink and itemShrink state props and also the handlers - By this version states, and handlers are working properly

---
+++ src/setup-app/flexbox-apps-list/flexbox.shrink-app.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import ComplexTitle from "../../components/complex-title";
+import FlexProp from "../../components/flex-prop.component";
+import { useAppContext } from "../../context";
+import { FlexShrinkPropertyWrapper } from "../flexbox.styled.components";
+
+/**Flexbox-grid-sass-and-animations app version 20 -
+ * FlexShrinkPropertyApp - Features:
+ * 
+ *       --> Building 'FlexShrinkPropertyApp'.
+ * 
+ *       --> Destructuring props from context. 
+ * 
+ *       --> Testing shrink and itemShrink state props 
+ *           and also the handlers.     
+ * 
+ * Note:  By this version 'flex-shrink' is harcoded,
+ * set it to '1'.
+ * 
+ * By this version states, and handlers are working properly
+ */
+
+const FlexShrinkPropertyApp = () => {
+
+    const { shrink, itemShrink, handleShrink, handleItemShrink } = useAppContext()
+
+    console.log(shrink)
+    console.log(itemShrink)
+
+    return(
+        <>
+        <ComplexTitle 
+            title={<h2>
+                <span className='flex-shrink-app'>flex shrink app:
+                </span></h2>}/>
+
+        <h2>'flex-shrink' property by defaul is on ( the default
+            value is 1 ), and this property is responsible for 
+            make responsive the width of the 'items' making them
+            shrink to adapt content to the width as the screen size
+            reduce - the use case for this property is when you 
+            have a content inside the 'item' that you want to
+            maintain the way that it looks no matter the changes of
+            the screen size so switching this prop to '0', and 
+            testing decreasing and increasing the size changes can be
+            appreciated</h2>
+
+            <select 
+                name="selection" 
+                id="selection" 
+                value={shrink} 
+                onChange={handleShrink}>
+                    {/**the first option with empty value is to 
+                     * avoid 'row' as default option - is a select 
+                     * tag behavior*/}
+                    <option value={""}>Select shrink ( Default 1 )</option>1
+                    <option value={0}> flex-shrink '0' ( Default 0 )</option>
+                    <option value={1}> flex-shrink '1'</option>
+            </select>
+
+            <select 
+                name="selection" 
+                id="selection" 
+                value={itemShrink} 
+                onChange={handleItemShrink}>
+                    {/**the first option with empty value is to 
+                     * avoid 'row' as default option - is a select 
+                     * tag behavior*/}
+                    <option value="">Select a shrink for an element</option>1
+                    <option value={1}> shrink the item # 1</option>
+                    <option value={2}> shrink the item # 2</option>
+                    <option value={3}> shrink the item # 3</option>
+                    <option value={4}> shrink the item # 4</option>
+                    <option value={5}> shrink the item # 5</option>
+            </select>
+            
+        <FlexShrinkPropertyWrapper>
+            <FlexProp />
+        </FlexShrinkPropertyWrapper>            
+            
+        </>
+    ) 
+}
+
+export default FlexShrinkPropertyApp;
