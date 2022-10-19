bcaaf777f3f834ad1bbf76dd529a70ad2ce07da6
Erick Rodriguez <erickrod54@gmail.com>
2022-10-19 06:43:12 -0400

    Flexbox-grid-sass-and-animations app version 24 - UIWhiteSpaceDesignFundamentalApp js file - Features: Building UIWhiteSpacesDesignFundamentalsApp basic Component

---
+++ src/setup-app/flexbox-apps-list/ui.white.space.design.fundamentals-app.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**Flexbox-grid-sass-and-animations app version 24 -
+ * UIWhiteSpaceDesingFundamentalsApp js file - Features:
+ * 
+ *      --> Building 'UIWhiteSpaceDesingFundamentalsApp'
+ *          basic Component  
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+const UIWhiteSpaceDesingFundamentalsApp = () => {
+
+    return(
+        <h2>UI White Space Desing Fundamentals App</h2>
+    )
+}
+
+export default UIWhiteSpaceDesingFundamentalsApp;
