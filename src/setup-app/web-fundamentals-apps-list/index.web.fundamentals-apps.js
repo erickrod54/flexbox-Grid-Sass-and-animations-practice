dc6b4ce1d9e787f0e58d4c50d6ac5372eda3dbfb
Erick Rodriguez <erickrod54@gmail.com>
2022-10-21 13:40:10 -0400

    Flexbox-grid-sass-and-animations app version 26 - index js file > web.fundamentals - Features: Adding UIWhite space to the index

---
+++ src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
@@ -0,0 +1,13 @@
+import UIWhiteSpaceApp from "./ui.white.space-app";
+
+/**Flexbox-grid-sass-and-animations app version 26 -
+ * index js file  > web.fundamentals-apps - 
+ * Features:
+ * 
+ *      --> Adding 'UIWhiteSpaceApp'  
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+export { UIWhiteSpaceApp } 
