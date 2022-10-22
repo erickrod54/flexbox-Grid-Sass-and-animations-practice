80c0f9014188c34f35a0c24dc07871512decc1e2
Erick Rodriguez <erickrod54@gmail.com>
2022-10-22 16:59:06 -0400

    Flexbix-grid-sass-and-animations app version 27 - index js file > web.fundamentals-app - Features: Adding WebDesignFundamentalsApp to the index

--- src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
+++ src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
@@ -1,13 +1,16 @@
 import UIWhiteSpaceApp from "./ui.white.space-app";
+import WebDesingFundamentalsApp from "./web.design.fundamentals-app";
 
-/**Flexbox-grid-sass-and-animations app version 26 -
+/**Flexbox-grid-sass-and-animations app version 27 -
  * index js file  > web.fundamentals-apps - 
  * Features:
  * 
- *      --> Adding 'UIWhiteSpaceApp'  
+ *      --> Adding 'WebDesingFundamentalsApp'  to the 
+ *          index 
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { UIWhiteSpaceApp } 
+
+export { UIWhiteSpaceApp, WebDesingFundamentalsApp } 
