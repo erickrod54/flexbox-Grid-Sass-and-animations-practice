c68d6a0bdf2e3c88a1bd8dc0ab454cab0f56b411
Erick Rodriguez <erickrod54@gmail.com>
2022-10-24 13:21:21 -0400

    Flexbox-grid-sass-and-animations app version 30 - index js file > web.fundamentals-apps - Features: Adding UIAlignmentApp to the index

--- src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
+++ src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
@@ -1,11 +1,13 @@
+import UIAlignmentApp from "./ui.alignment-app";
 import UIWhiteSpaceApp from "./ui.white.space-app";
 import WebDesingFundamentalsApp from "./web.design.fundamentals-app";
 
-/**Flexbox-grid-sass-and-animations app version 27 -
+
+/**Flexbox-grid-sass-and-animations app version 30 -
  * index js file  > web.fundamentals-apps - 
  * Features:
  * 
- *      --> Adding 'WebDesingFundamentalsApp'  to the 
+ *      --> Adding 'UIAlignmentApp'  to the 
  *          index 
  * 
  * Note: By this version the apps imports are simplyfied
@@ -13,4 +15,4 @@ import WebDesingFundamentalsApp from "./web.design.fundamentals-app";
  */
 
 
-export { UIWhiteSpaceApp, WebDesingFundamentalsApp } 
+export { UIWhiteSpaceApp, WebDesingFundamentalsApp, UIAlignmentApp } 
