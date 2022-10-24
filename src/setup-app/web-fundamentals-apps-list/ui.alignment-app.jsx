736bcc446f953bbc7d78c239a384044689935699
Erick Rodriguez <erickrod54@gmail.com>
2022-10-24 13:24:58 -0400

    Flexbox-grid-sass-and-animations app version 30 - UIAlignmentApp - Features: Building UIAlignmentApp basic component

---
+++ src/setup-app/web-fundamentals-apps-list/ui.alignment-app.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**Flexbox-grid-sass-and-animations app version 30 -
+ * UIAlignmentApp - Features:
+ * 
+ *      -->Building 'UIAlignmentApp' basic 
+ *         Component
+ * 
+ * Note: This features added due to the last app
+ */
+
+const UIAlignmentApp = () => {
+
+    return(
+        <h2>ui Alignment App</h2>
+    )
+}
+
+export default UIAlignmentApp;
