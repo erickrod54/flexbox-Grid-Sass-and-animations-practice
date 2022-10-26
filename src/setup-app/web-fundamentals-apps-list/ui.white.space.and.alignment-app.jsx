405d844846d6948319df5cabb68cbbd1ae30986d
Erick Rodriguez <erickrod54@gmail.com>
2022-10-26 13:08:09 -0400

    Flexbox-grid-sass-and-animations app version 31 - UIWhitesapceAndAlignmentApp - Features: Building a basic UIWhitesapceAndAlignment Component

---
+++ src/setup-app/web-fundamentals-apps-list/ui.white.space.and.alignment-app.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**Flexbox-grid-sass-and-animations app version 31 -
+ * 'UIWhitespaceAndAlignmentApp' - Features:
+ * 
+ *      --> Building basic 'UIWhitespaceAndAlignmentApp'  
+ *          Component.
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+const UIWhitespaceAndAlignmentApp = () => {
+
+    return(
+        <h2>UI white space and alignment app</h2>
+    )
+}
+
+export default UIWhitespaceAndAlignmentApp;
