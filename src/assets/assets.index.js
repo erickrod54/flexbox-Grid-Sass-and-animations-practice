9d2f3154125a854e8e84a6be8c44022a85461a7c
Erick Rodriguez <erickrod54@gmail.com>
2022-10-28 10:53:46 -0400

    Flexbox-grid-sass-and-animations app version 33 - assets > index - Features: Importing and exporting images

---
+++ src/assets/assets.index.js
@@ -0,0 +1,15 @@
+import dog from './dog.jpg';
+import moose from './moose.jpg';
+import owl from './owl.jpg';
+
+/**Flexbox-grid-sass-and-animations app version 33 -
+ * 'assets' > index - Features:
+ * 
+ *      --> Importing and exporting images.
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+
+export { dog, moose, owl }
