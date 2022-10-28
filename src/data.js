74801466f5eb3e9fb63ea55e62eefbfb4545cad9
Erick Rodriguez <erickrod54@gmail.com>
2022-10-28 10:46:11 -0400

    Flexbox-grid-sass-and-animations app version 33 - data js - Features: Adding assets > images to UIWhitespaceAndAlignData

--- src/data.js
+++ src/data.js
@@ -1,10 +1,10 @@
-/**Flexbox-grid-sass-and-animations app version 31 -
+import { dog, moose, owl } from './assets/assets.index'
+
+/**Flexbox-grid-sass-and-animations app version 33 -
  * data js - Features:
  * 
- *      --> Building 'UIWhitespaceAndAlignmentData'.
- * 
- *      --> Adding 'UIWhitespaceAndAlignmentApp' to
- *          the 'appWebFundamentalsData'
+ *      --> Adding assets > images to  
+ *         'UIWhitespaceAndAlignmentData'.
  * 
  * Note: 'Order Property' (this is the first items property)
  * is properties applied directly to items inside of 
@@ -387,18 +387,21 @@ export const uiAlignmentData = [
 export const UIWhitespaceAndAlignmentData = [
     {
         id:1,
+        image:dog,
         date: 'some date 1',
         headline:'some headline',
         text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
         id:2,
+        image:moose,
         date: 'some date 2',
         headline:'some headline',
         text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
         id:3,
+        image:owl,
         date: 'some date 3',
         headline:'some headline',
         text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
