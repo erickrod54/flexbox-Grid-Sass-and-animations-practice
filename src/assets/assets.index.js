1a3185229c8c4867800940e1dfd6a46347f2eafd
Erick Rodriguez <erickrod54@gmail.com>
2022-11-05 00:20:28 -0400

    Flexbox-grid-sass-and-animations app version 37 - assets > index - Features: Importing and exporti ng sheep, bird, and tiger assets

--- src/assets/assets.index.js
+++ src/assets/assets.index.js
@@ -1,15 +1,19 @@
 import dog from './dog.jpg';
 import moose from './moose.jpg';
 import owl from './owl.jpg';
+import sheep from './sheep.jpg';
+import bird from './bird.jpg';
+import tiger from './tiger.jpg'
 
-/**Flexbox-grid-sass-and-animations app version 33 -
+/**Flexbox-grid-sass-and-animations app version 37 -
  * 'assets' > index - Features:
  * 
- *      --> Importing and exporting images.
+ *      --> Importing and exporting 'sheep', 'bird', 
+ *          and 'tiger' assets.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
 
-export { dog, moose, owl }
+export { dog, moose, owl, sheep, bird, tiger }
