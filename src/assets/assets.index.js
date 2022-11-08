191b9fbe8ac6331f8a71d5e1d07d6dcd62a7cba2
Erick Rodriguez <erickrod54@gmail.com>
2022-11-08 12:43:06 -0500

    Flexbox-grid-sass-and-animations app version 39 - assets > index - Features: Importing and exporting mrcat, mrgiraffe, mrsmallwolf assets

--- src/assets/assets.index.js
+++ src/assets/assets.index.js
@@ -4,16 +4,19 @@ import owl from './owl.jpg';
 import sheep from './sheep.jpg';
 import bird from './bird.jpg';
 import tiger from './tiger.jpg'
+import mrcat from './mr.cat.jpg';
+import mrgiraffe from './mr.giraffe.jpg';
+import mrsmallwolf from './mr.small.wolf.jpg';
 
-/**Flexbox-grid-sass-and-animations app version 37 -
+/**Flexbox-grid-sass-and-animations app version 39 -
  * 'assets' > index - Features:
  * 
- *      --> Importing and exporting 'sheep', 'bird', 
- *          and 'tiger' assets.
+ *      --> Importing and exporting 'mrcat', 'mrgiraffe', 
+ *          'mrsmallwolf' assets.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
 
-export { dog, moose, owl, sheep, bird, tiger }
+export { dog, moose, owl, sheep, bird, tiger, mrcat, mrgiraffe, mrsmallwolf }
