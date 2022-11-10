1b31d286a84ae291c748a0ef95a630735c15bcc5
Erick Rodriguez <erickrod54@gmail.com>
2022-11-09 20:18:41 -0500

    Flexbox-grid-sass-and-animations app version 40 - UITypograhyFont - Features: Building UITypographyFont to apply global fonts to the UITypographyApp

---
+++ src/setup-app/web-fundamentals-apps-list/ui.typography.global.fonts.jsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+/**Flexbox-grid-sass-and-animations app version 40 -
+ * 'UITypographyFont' - Features:
+ * 
+ *      --> Building 'UITypographyFont' to apply global
+ *          fonts to the 'UITypographyApp'.
+ * 
+ * Note: by this version is the is everything done 
+ * in this component.
+ */
+
+export const UITypographyFont = createGlobalStyle`
+    body{
+        font-family: 'Cinzel', serif;
+        font-family: 'Kalam', cursive;
+    }
+`
