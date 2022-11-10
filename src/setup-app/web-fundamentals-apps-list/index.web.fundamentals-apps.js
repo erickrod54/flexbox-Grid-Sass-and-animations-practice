a7004822e2d078d3e47a05d54b7fec17c14ed1da
Erick Rodriguez <erickrod54@gmail.com>
2022-11-10 11:05:01 -0500

    Flexbox-grid-sass-and-animations app version 41 - index js file > web.fundamentals-apps - Features: Adding UIColorApp to the index

--- src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
+++ src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
@@ -1,4 +1,5 @@
 import UIAlignmentApp from "./ui.alignment-app";
+import UIColorApp from "./ui.color-app";
 import UIContrastAndScaleApp from "./ui.contrast.and.scale-app";
 import UITypographyApp from "./ui.typography-app";
 import UIWhiteSpaceApp from "./ui.white.space-app";
@@ -6,18 +7,16 @@ import UIWhitespaceAndAlignmentApp from "./ui.white.space.and.alignment-app";
 import WebDesingFundamentalsApp from "./web.design.fundamentals-app";
 
 
-/**Flexbox-grid-sass-and-animations app version 40 -
+/**Flexbox-grid-sass-and-animations app version 41 -
  * index js file  > web.fundamentals-apps - 
  * Features:
  * 
- *      --> Adding 'UITypographyApp'  to 
+ *      --> Adding 'UIColorApp'  to 
  *          the index 
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
-
-
 
 export { 
          UIWhiteSpaceApp,
@@ -25,5 +24,6 @@ export {
          UIAlignmentApp,
          UIWhitespaceAndAlignmentApp,
          UIContrastAndScaleApp,
-         UITypographyApp
+         UITypographyApp,
+         UIColorApp
         } 
