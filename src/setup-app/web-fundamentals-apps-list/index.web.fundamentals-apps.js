bda0e535c6c9118d9bc10a8fb6e99adad8f55b92
Erick Rodriguez <erickrod54@gmail.com>
2022-11-17 21:32:00 -0500

    Flexbox-grid-sass-and-animations app version 43 - index js file > web.fundamentals-apps - Features: Adding UIFormApp to the index, Adding UIVisualHierarchy to the index

--- src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
+++ src/setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps.js
@@ -1,17 +1,22 @@
 import UIAlignmentApp from "./ui.alignment-app";
 import UIColorApp from "./ui.color-app";
 import UIContrastAndScaleApp from "./ui.contrast.and.scale-app";
+import UIFormApp from "./ui.form-app";
 import UITypographyApp from "./ui.typography-app";
+import UIVisualHierarchyApp from "./ui.visual.hierarchy-app";
 import UIWhiteSpaceApp from "./ui.white.space-app";
 import UIWhitespaceAndAlignmentApp from "./ui.white.space.and.alignment-app";
 import WebDesingFundamentalsApp from "./web.design.fundamentals-app";
 
 
-/**Flexbox-grid-sass-and-animations app version 41 -
+/**Flexbox-grid-sass-and-animations app version 43 -
  * index js file  > web.fundamentals-apps - 
  * Features:
  * 
- *      --> Adding 'UIColorApp'  to 
+ *      --> Adding 'UIFormApp'  to 
+ *          the index 
+ * 
+ *      --> Adding 'UIVisualHierarchyApp' to
  *          the index 
  * 
  * Note: By this version the apps imports are simplyfied
@@ -25,5 +30,7 @@ export {
          UIWhitespaceAndAlignmentApp,
          UIContrastAndScaleApp,
          UITypographyApp,
-         UIColorApp
+         UIColorApp,
+         UIFormApp,
+         UIVisualHierarchyApp
         } 
