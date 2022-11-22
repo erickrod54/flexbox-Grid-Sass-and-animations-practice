b993823e0c343ab556fc166c8cf11942e1241713
Erick Rodriguez <erickrod54@gmail.com>
2022-11-22 11:30:24 -0500

    Flexbox-grid-sass-and-animations app version 45 - WebDesignFundamentalsApp - Features: Taking off unused Components

--- src/setup-app/web-fundamentals-apps-list/web.design.fundamentals-app.jsx
+++ src/setup-app/web-fundamentals-apps-list/web.design.fundamentals-app.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
 import AppElementWebDesignFundamentals from "../../components/app.element.web.fundamentals.component";
 import ComplexTitle from "../../components/complex-title";
 import { useAppContext } from "../../context";
 import { HomeWrapper, NavbarWrapper } from "../../pages/home-page";
 
-/**Flexbox-grid-sass-and-animations app version 27 -
+/**Flexbox-grid-sass-and-animations app version 45 -
  * WebDesingFundamentalsApp - Features:
  * 
- *      --> Building 'WebDesingFundamentalsApp'
- * 
- *      --> Destructuring the data from the context.
- * 
- *      --> Building the return. 
+ *      --> Taking off unused Components
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
