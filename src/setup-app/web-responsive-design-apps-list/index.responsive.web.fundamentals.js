ca0a4b3576821a9ad547a6c33d52d6be015a206f
Erick Rodriguez <erickrod54@gmail.com>
2022-11-23 12:56:09 -0500

    Flexbox-grid-sass-and-animations app version 45 - index > responsive.web.design.fundamentals - Features: Adding ResponsiveWebDesignApp to the index

---
+++ src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
@@ -0,0 +1,14 @@
+import ResponsiveWebDesignApp from "./responsive.web.fundamentals-app";
+
+/**Flexbox-grid-sass-and-animations app version 45 -
+ * index > responsive.web.design.fundamentals 
+ * - Features:
+ * 
+ *      --> Adding 'ResponsiveWebDesignApp' to
+ *          the index.
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+export { ResponsiveWebDesignApp }
