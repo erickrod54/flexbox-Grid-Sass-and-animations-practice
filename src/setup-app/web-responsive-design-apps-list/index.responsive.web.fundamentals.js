4abb97849ec8be99b41aaffd135020476007eaeb
Erick Rodriguez <erickrod54@gmail.com>
2022-11-24 13:07:45 -0500

    Flexbox-grid-sass-and-animations app version 46 - index > responsive.web.design.fundamentals - Features: Adding UnitsAndProps to the index

--- src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
+++ src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
@@ -1,14 +1,15 @@
 import ResponsiveWebDesignApp from "./responsive.web.fundamentals-app";
+import UnitsAndProps from "./units.and.props.component-app";
 
-/**Flexbox-grid-sass-and-animations app version 45 -
+/**Flexbox-grid-sass-and-animations app version 46 -
  * index > responsive.web.design.fundamentals 
  * - Features:
  * 
- *      --> Adding 'ResponsiveWebDesignApp' to
+ *      --> Adding 'UnitsAndProps' to
  *          the index.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { ResponsiveWebDesignApp }
+export { ResponsiveWebDesignApp, UnitsAndProps }
