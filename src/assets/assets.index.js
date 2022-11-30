c81b63fbffaf19f85caba0d52971936457a1ee8c
Erick Rodriguez <erickrod54@gmail.com>
2022-11-30 12:39:35 -0500

    Flexbox-grid-sass-and-animations app version 49 - assets > index - Features: Importing and exporting maxwidthimage asset

--- src/assets/assets.index.js
+++ src/assets/assets.index.js
@@ -7,16 +7,17 @@ import tiger from './tiger.jpg'
 import mrcat from './mr.cat.jpg';
 import mrgiraffe from './mr.giraffe.jpg';
 import mrsmallwolf from './mr.small.wolf.jpg';
+import maxwidthimage from './max-width-aaset.png'
 
-/**Flexbox-grid-sass-and-animations app version 39 -
+/**Flexbox-grid-sass-and-animations app version 49 -
  * 'assets' > index - Features:
  * 
- *      --> Importing and exporting 'mrcat', 'mrgiraffe', 
- *          'mrsmallwolf' assets.
+ *      --> Importing and exporting 'maxwidthimage' 
+ *          assets.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
 
-export { dog, moose, owl, sheep, bird, tiger, mrcat, mrgiraffe, mrsmallwolf }
+export { dog, moose, owl, sheep, bird, tiger, mrcat, mrgiraffe, mrsmallwolf, maxwidthimage }
