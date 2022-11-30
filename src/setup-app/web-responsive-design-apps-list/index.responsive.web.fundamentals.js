d8a1617e2ef55af5ac59191bef34c9de912245d9
Erick Rodriguez <erickrod54@gmail.com>
2022-11-30 12:58:29 -0500

    Flexbox-grid-sass-and-animations app version 49 - index > responsive.web.design.fundamentals - Features: Adding ViewPortUnits to the index

--- src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
+++ src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
@@ -1,15 +1,16 @@
 import ResponsiveWebDesignApp from "./responsive.web.fundamentals-app";
 import UnitsAndProps from "./units.and.props.component-app";
+import ViewPortUnits from "./viewport.units.component";
 
-/**Flexbox-grid-sass-and-animations app version 46 -
+/**Flexbox-grid-sass-and-animations app version 49 -
  * index > responsive.web.design.fundamentals 
  * - Features:
  * 
- *      --> Adding 'UnitsAndProps' to
+ *      --> Adding 'ViewPortUnits' to
  *          the index.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { ResponsiveWebDesignApp, UnitsAndProps }
+export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits }
