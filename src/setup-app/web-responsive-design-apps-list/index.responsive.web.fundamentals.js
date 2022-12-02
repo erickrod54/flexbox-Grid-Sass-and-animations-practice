284ec71810157c446258c3df6d000e66b77fc5a4
Erick Rodriguez <erickrod54@gmail.com>
2022-12-02 03:55:33 -0500

    Flexbox-grid-sass-and-animations app version 51 - index > responsive.web.design.fundamentals - Features: Adding MediaQueriesComponent to the index

--- src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
+++ src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
@@ -1,16 +1,17 @@
+import MediaQueriesComponent from "./media.queries.component";
 import ResponsiveWebDesignApp from "./responsive.web.fundamentals-app";
 import UnitsAndProps from "./units.and.props.component-app";
 import ViewPortUnits from "./viewport.units.component";
 
-/**Flexbox-grid-sass-and-animations app version 49 -
+/**Flexbox-grid-sass-and-animations app version 51 -
  * index > responsive.web.design.fundamentals 
  * - Features:
  * 
- *      --> Adding 'ViewPortUnits' to
+ *      --> Adding 'MediaQueriesComponent' to
  *          the index.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits }
+export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits, MediaQueriesComponent }
