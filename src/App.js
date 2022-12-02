764f541520b6e52ac7e28dc289eb3002efcbc226
Erick Rodriguez <erickrod54@gmail.com>
2022-12-02 03:56:49 -0500

    Flexbox-grid-sass-and-animations app version 51 - App js file - Features: Adding MediaQueries and building a route for it

--- src/App.js
+++ src/App.js
@@ -31,13 +31,12 @@ import { UIWhiteSpaceApp,
          UIVisualHierarchyApp,
          UIDesignChallenge} from './setup-app/web-fundamentals-apps-list/index.web.fundamentals-apps' 
 
-import { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits } from './setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals';
+import { MediaQueriesComponent, ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits } from './setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals';
 
-
-/**Flexbox-grid-sass-and-animations app version 49 -
+/**Flexbox-grid-sass-and-animations app version 51 -
  * App js file - Features:
  * 
- *       --> Adding 'ViewPortUnits' and 
+ *       --> Adding 'MediaQueries' and 
  *           building a route for it. 
  * 
  * Note: starting to build the ui color 
@@ -78,6 +77,7 @@ function App() {
         <Route path='/apps/web-responsive-design-fundamentals-app' element={<ResponsiveWebDesignApp />}/>
         <Route path='/apps/web-responsive-design-fundamentals-app/units-and-props' element={<UnitsAndProps />}/>
         <Route path='/apps/web-responsive-design-fundamentals-app/view-port-units' element={<ViewPortUnits />}/>
+        <Route path='/apps/web-responsive-design-fundamentals-app/media-queries' element={<MediaQueriesComponent />}/>  
         </Route>
       </Routes>
     </Router>
