740c9e8c907ba3cc73f1fb3d5e6907f4176f7d49
Erick Rodriguez <erickrod54@gmail.com>
2022-12-02 07:05:47 -0500

    Flexbox-grid-sass-and-animations app version 52 - index > responsive.web.design.fundamentals - Features: Adding WeatherUiApp to the index

--- src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
+++ src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
@@ -2,16 +2,18 @@ import MediaQueriesComponent from "./media.queries.component";
 import ResponsiveWebDesignApp from "./responsive.web.fundamentals-app";
 import UnitsAndProps from "./units.and.props.component-app";
 import ViewPortUnits from "./viewport.units.component";
+import WheaterUiApp from "./wheater.ui-app";
 
-/**Flexbox-grid-sass-and-animations app version 51 -
+
+/**Flexbox-grid-sass-and-animations app version 52 -
  * index > responsive.web.design.fundamentals 
  * - Features:
  * 
- *      --> Adding 'MediaQueriesComponent' to
+ *      --> Adding 'WheaterUiApp' to
  *          the index.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits, MediaQueriesComponent }
+export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits, MediaQueriesComponent, WheaterUiApp }
