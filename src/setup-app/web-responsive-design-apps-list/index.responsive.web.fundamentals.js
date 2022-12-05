588777c9e84fcfe5e125f4c8ddaf74fabb217e1e
Erick Rodriguez <erickrod54@gmail.com>
2022-12-04 21:17:59 -0500

    Flexbox-grid-sass-and-animations app version 54 - index > responsive.web.design.fundamentals - Features: Adding ViewportUnitsCases to the index

--- src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
+++ src/setup-app/web-responsive-design-apps-list/index.responsive.web.fundamentals.js
@@ -2,18 +2,18 @@ import MediaQueriesComponent from "./media.queries.component";
 import ResponsiveWebDesignApp from "./responsive.web.fundamentals-app";
 import UnitsAndProps from "./units.and.props.component-app";
 import ViewPortUnits from "./viewport.units.component";
+import ViewportUnitsCases from "./viewport.units.usecases.component";
 import WheaterUiApp from "./wheater.ui-app";
 
-
-/**Flexbox-grid-sass-and-animations app version 52 -
+/**Flexbox-grid-sass-and-animations app version 54 -
  * index > responsive.web.design.fundamentals 
  * - Features:
  * 
- *      --> Adding 'WheaterUiApp' to
+ *      --> Adding 'ViewportUnitsCases' to
  *          the index.
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits, MediaQueriesComponent, WheaterUiApp }
+export { ResponsiveWebDesignApp, UnitsAndProps, ViewPortUnits, MediaQueriesComponent, WheaterUiApp, ViewportUnitsCases }
