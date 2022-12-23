c9dc4d220214d46af26a16c584bb8079a1a4a700
Erick Rodriguez <erickrod54@gmail.com>
2022-12-22 22:31:30 -0500

    Flexbox-grid-sass-and-animations app version 58 - data js - Features: Building gridIntroData

--- src/data.js
+++ src/data.js
@@ -14,12 +14,11 @@ import {
     duck
 } from './assets/assets.index'
 
-/**Flexbox-grid-sass-and-animations app version 57 -
+/**Flexbox-grid-sass-and-animations app version 58 -
  * data js - Features:
  *
  *  
- *      --> Building 'cssGridlayoutData' and adding 
- *          it to the 'AppLinks' list
+ *      --> Building 'gridIntroData'
  *      
  * Note: this data is going to be passed to build 
  * uiColorApp.
@@ -640,6 +639,49 @@ export const cssGridlayoutData = [
         url:'/apps/web-responsive-design-fundamentals-app'  
       },
        */
+]
+
+export const gridIntroData = [
+    {
+      id:1,
+      name:'item-1'
+    },
+    {
+      id:2,
+      name:'item-2'
+    },
+    {
+      id:3,
+      name:'item-3'
+    },
+    {
+      id:4,
+      name:'item-4'
+    },
+    {
+      id:5,
+      name:'item-5'
+    },
+    {
+      id:6,
+      name:'item-6'
+    },
+    {
+      id:7,
+      name:'item-7'
+    },
+    {
+      id:8,
+      name:'item-8'
+    },
+    {
+      id:9,
+      name:'item-9'
+    },
+    {
+      id:10,
+      name:'item-10'
+    }
 ]
 
 export const cloud = <FontAwesomeIcon icon={faCloud}/>
