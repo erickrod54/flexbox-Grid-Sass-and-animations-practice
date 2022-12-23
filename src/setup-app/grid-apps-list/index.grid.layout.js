2f43b7ee8d1bec0236763533b561bff1b616b52d
Erick Rodriguez <erickrod54@gmail.com>
2022-12-22 22:37:11 -0500

    Flexbox-grid-sass-and-animations app version 58 - GridLayout > index js - Features: Importing and providing GridLayout and GridIntro

---
+++ src/setup-app/grid-apps-list/index.grid.layout.js
@@ -0,0 +1,14 @@
+import GridIntro from "./grid.intro.app";
+import GridLayout from "./grid.layout";
+
+/**Flexbox-grid-sass-and-animations app version 58 -
+ * GridLayout > index js - Features:
+ * 
+ *      --> Importing and providing 'GridLayout', 
+ *          and 'GridIntro' 
+ * 
+ * Note: By this version the apps imports are simplyfied
+ * to use in App js for routing that is need it.
+ */
+
+export { GridLayout, GridIntro }
