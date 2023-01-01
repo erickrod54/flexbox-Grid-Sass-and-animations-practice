c82b5efff6e693fd27935828dd13f49b8a0a9719
Erick Rodriguez <erickrod54@gmail.com>
2023-01-01 17:01:23 -0500

    Flexbox-grid-sass-and-animations app version 60.11 - GridLayout > index - Features: Importing and Providing GridItemPosition

--- src/setup-app/grid-apps-list/index.grid.layout.js
+++ src/setup-app/grid-apps-list/index.grid.layout.js
@@ -1,14 +1,16 @@
 import GridIntro from "./grid.intro.app";
+import GridItemsPosition from "./grid.items.position";
 import GridLayout from "./grid.layout";
 import RepeatAndFrUnit from "./repeat.fr.unit";
 
-/**Flexbox-grid-sass-and-animations app version 60.6 -
+/**Flexbox-grid-sass-and-animations app version 60.11 -
  * GridLayout > index js - Features:
  * 
- *      --> Importing and providing 'RepeatAndFrUnit' 
+ *      --> Importing and providing 'gridItemsPosition' 
  * 
  * Note: By this version the apps imports are simplyfied
  * to use in App js for routing that is need it.
  */
 
-export { GridLayout, GridIntro, RepeatAndFrUnit}
+
+export { GridLayout, GridIntro, RepeatAndFrUnit, GridItemsPosition}
