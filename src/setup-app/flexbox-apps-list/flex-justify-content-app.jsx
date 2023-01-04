7886e3498f4bd782c973618ca4c1400eebe79852
Erick Rodriguez <erickrod54@gmail.com>
2023-01-04 14:05:19 -0500

    Flexbox-grid-sass-and-animations app version 60.12 - JustifyContentApp - Features: Implementing SelectionForm, Building and testing data, pending to migrate data

--- src/setup-app/flexbox-apps-list/flex-justify-content-app.jsx
+++ src/setup-app/flexbox-apps-list/flex-justify-content-app.jsx
@@ -4,22 +4,52 @@ import FlexProp from "../../components/flex-prop.component";
 import { JustifyContentAppWrapper } from "../flexbox.styled.components";
 import JustifyContent from '../../components/justifyflex-content.component';
 import { useFlexboxContext } from "../../apps-context/flexbox.context";
+import SelectionForm from "../../components/selection.form.component";
 
-/**Flexbox-grid-sass-and-animations app version 60.02 -
+/**Flexbox-grid-sass-and-animations app version 60.12 -
  * 'JustifyContentApp' - Features:
  * 
- *      --> Redirecting states and features by 
- *          'useFlexboxContext()'.
+ *       --> Implementing 'SelectionForm'
  * 
- *      --> Changing versioning to 2 digits.
+ *      --> Building and testing data 
  * 
- * Note: By this version everything is done in
- * this component.
+ * Note: Pending to migrate data
  */
 
 const JustifyContentApp = () => {
 
     const { justify, handleJustify } = useFlexboxContext()
+
+    const FlexJustifyData = [
+        {
+          id:1,
+          value:"none"      
+        },
+        {
+          id:2,
+          value:'flex-start'      
+        },
+        {
+          id:3,
+          value:'flex-end'      
+        },
+        {
+          id:4,
+          value:'center'      
+        },
+        {
+          id:5,
+          value:'space-between'      
+        },
+        {
+          id:6,
+          value:'space-around'      
+        },
+        {
+          id:7,
+          value:'space-evenly'      
+        },
+    ]
 
     return(
         <>
@@ -27,22 +57,8 @@ const JustifyContentApp = () => {
         title={<h2><span 
             className='justify-app'>Justify Content App:</span></h2>}/>
         <JustifyContent justify={justify} /> 
-        <select 
-                name="selection" 
-                id="selection" 
-                value={justify} 
-                onChange={handleJustify}>
-                    {/**the first option with empty value is to 
-                     * avoid 'row' as default option - is a select 
-                     * tag behavior*/}
-                    <option value="">Select a justify</option>1
-                    <option value='flex-start'>flex-start</option>
-                    <option value='flex-end'>flex-end</option>
-                    <option value='center'>center</option>
-                    <option value='space-between'>space-between</option>
-                    <option value='space-around'>space-around</option>
-                    <option value='space-evenly'>space-evenly</option>
-                </select>
+
+        <SelectionForm justify={justify} handler={handleJustify} propertiesArray={FlexJustifyData} propertyname={'justify-content'} />
 
         <JustifyContentAppWrapper justify={justify}>
         <FlexProp/>
