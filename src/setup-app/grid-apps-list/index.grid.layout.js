4ce63ac3a61fe696403bea40b10019b089d6230f
Erick Rodriguez <erickrod54@gmail.com>
2023-01-08 21:30:03 -0500

    Flexbox-grid-sass-and-animations app version 60.15 - GridLayout > index - Features: Importing and providing GridItemProperties, GridContainerProperties, and GridMaxMinContent

--- src/setup-app/grid-apps-list/index.grid.layout.js
+++ src/setup-app/grid-apps-list/index.grid.layout.js
@@ -1,16 +1,30 @@
 import GridIntro from "./grid.intro.app";
 import GridItemsPosition from "./grid.items.position";
 import GridLayout from "./grid.layout";
+import GridTemplateArea from "./grid.template.area.property";
 import RepeatAndFrUnit from "./repeat.fr.unit";
+import GridContainerProperties from './grid.justifify.align.place.items.properties';
+import GridAutoRowFlow from "./grid.auto.rows.flow.colums";
+import GridItemProperties from "./grid.item.properties";
+import GridMaxMinContent from "./grid.max.min.content.function";
 
-/**Flexbox-grid-sass-and-animations app version 60.11 -
+/**Flexbox-grid-sass-and-animations app version 60.16 -
  * GridLayout > index js - Features:
  * 
- *      --> Importing and providing 'gridItemsPosition' 
+ *      --> Importing and providing 'GridItemProperties'. 
  * 
- * Note: By this version the apps imports are simplyfied
- * to use in App js for routing that is need it.
+ *      --> Importing and providing 'GridContainerProperties'.
+ * 
+ *      -> Importing and providing 'GridMaxMinContent'.
+ * 
+ * Note: Changing 'GridItemProperties' to GridContainerProperties
+ * because 'grid.justifify.align.place.items.properties' are 
+ * conatiner properties.
+ * 
+ * And naming 'GridItemProperties' for 'grid.item.properties'
+ * component
  */
 
 
-export { GridLayout, GridIntro, RepeatAndFrUnit, GridItemsPosition}
+ 
+export { GridLayout, GridIntro, RepeatAndFrUnit, GridItemsPosition, GridTemplateArea, GridContainerProperties, GridAutoRowFlow, GridItemProperties, GridMaxMinContent }
