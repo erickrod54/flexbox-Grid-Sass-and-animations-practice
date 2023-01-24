1f0a7caf372135520f7bd1cbe51f0b755e6d99cb
Erick Rodriguez <erickrod54@gmail.com>
2023-01-23 22:35:14 -0500

    Flexbox-grid-sass-and-animations app version 61.12 - GridLayout > index js - Features: Importing and providing GridAutoFillFit

--- src/setup-app/grid-apps-list/index.grid.layout.js
+++ src/setup-app/grid-apps-list/index.grid.layout.js
@@ -7,15 +7,12 @@ import GridContainerProperties from './grid.justifify.align.place.items.properti
 import GridAutoRowFlow from "./grid.auto.rows.flow.colums";
 import GridItemProperties from "./grid.item.properties";
 import GridMaxMinContent from "./grid.max.min.content.function";
+import GridAutoFillFit from "./autofill.and.autofit.values";
 
-/**Flexbox-grid-sass-and-animations app version 60.16 -
+/**Flexbox-grid-sass-and-animations app version 61.12 -
  * GridLayout > index js - Features:
  * 
- *      --> Importing and providing 'GridItemProperties'. 
- * 
- *      --> Importing and providing 'GridContainerProperties'.
- * 
- *      -> Importing and providing 'GridMaxMinContent'.
+ *      --> Importing and providing 'GridAutoFillFit'. 
  * 
  * Note: Changing 'GridItemProperties' to GridContainerProperties
  * because 'grid.justifify.align.place.items.properties' are 
@@ -27,4 +24,4 @@ import GridMaxMinContent from "./grid.max.min.content.function";
 
 
  
-export { GridLayout, GridIntro, RepeatAndFrUnit, GridItemsPosition, GridTemplateArea, GridContainerProperties, GridAutoRowFlow, GridItemProperties, GridMaxMinContent }
+export { GridLayout, GridIntro, RepeatAndFrUnit, GridItemsPosition, GridTemplateArea, GridContainerProperties, GridAutoRowFlow, GridItemProperties, GridMaxMinContent, GridAutoFillFit }
